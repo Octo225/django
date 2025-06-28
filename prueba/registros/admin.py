cbcd05aa4de9f6e73c0cb723b5623ab78f3519c4
Octo225 <luciosgonzalea@gmail.com>
2025-06-28 13:07:15 -0600

    commit

--- prueba/registros/admin.py
+++ prueba/registros/admin.py
@@ -1,8 +1,23 @@
 from django.contrib import admin
 from .models import Alumnos
+from .models import Comentario
+
+
 # Register your models here.
 
 class AdministrarModelo(admin.ModelAdmin):
     readonly_fields = ('created','updated')
+    list_display= ('matricula', 'nombre', 'carrera', 'turno')
+    search_fields=('matricula', 'nombre', 'carrera', 'turno')
+    date_hierarchy= 'created'
+    list_filter=('carrera','turno')
 
 admin.site.register(Alumnos, AdministrarModelo)
+
+class AdministrarComentarios(admin.ModelAdmin):
+    readonly_fields = ('created','id')
+    list_display= ('id', 'coment')
+    search_fields=('id', 'created')
+    date_hierarchy= 'created'
+
+admin.site.register(Comentario, AdministrarComentarios)
--- prueba/registros/models.py
+++ prueba/registros/models.py
@@ -1,5 +1,5 @@
 from django.db import models
-
+from ckeditor.fields import RichTextField
 # Create your models here.
 
 
@@ -17,3 +17,18 @@ class Alumnos(models.Model):
         ordering = ["-created"]
     def __str__(self):
         return self.nombre
+
+class Comentario(models.Model):
+    id= models.AutoField(primary_key=True,verbose_name="Clave")
+    alumno= models.ForeignKey(Alumnos,on_delete=models.CASCADE,verbose_name="Alumno")
+    created=models.DateTimeField(auto_now_add=True,verbose_name="Registrado")
+    coment=RichTextField(verbose_name="Comentario")
+
+    class Meta:
+        verbose_name='Comentario'
+        verbose_name_plural='Comentarios'
+        ordering = ["-created"]
+    def __str__(self):
+        return self.coment
+
+    
