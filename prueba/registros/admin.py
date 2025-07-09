c3858b67b09003b29aebfae00cd1bd79f71f80a2
Octo225 <luciosgonzalea@gmail.com>
2025-07-09 17:32:36 -0600

    permisos de admin

--- prueba/registros/admin.py
+++ prueba/registros/admin.py
@@ -13,13 +13,25 @@ class AdministrarModelo(admin.ModelAdmin):
     date_hierarchy= 'created'
     list_filter=('carrera','turno')
 
+    def get_readonly_fields(self, request, obj=None):
+        if request.user.groups.filter(name="Usuarios").exists():
+            return ('created', 'updated', 'matricula', 'carrera','turno')
+        else:
+            return('created', 'updated')
+
 admin.site.register(Alumnos, AdministrarModelo)
 
 class AdministrarComentarios(admin.ModelAdmin):
     readonly_fields = ('created','id')
     list_display= ('id', 'coment')
     search_fields=('id', 'created')
-    date_hierarchy= 'created'
+    date_hierarchy= ('created')
+
+    def get_readonly_fields(self, request, obj=None):
+        if request.user.groups.filter(name="Comentarios").exists():
+            return ('created', 'alumno')
+        else:
+            return('created')
 
 admin.site.register(Comentario, AdministrarComentarios)
 
