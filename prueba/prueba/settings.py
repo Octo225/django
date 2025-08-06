d5f07730f45c5f233a21fcfccba20e933402ea8e
Octo225 <luciosgonzalea@gmail.com>
2025-08-05 19:39:54 -0600

    preparacion para host

--- prueba/prueba/settings.py
+++ prueba/prueba/settings.py
@@ -85,12 +85,8 @@ WSGI_APPLICATION = 'prueba.wsgi.application'
 
 DATABASES ={
     'default': {
-        'ENGINE':'django.db.backends.mysql',
-        'NAME':'ejemplo',
-        'USER':'root',
-        'PASSWORD':'root',
-        'HOST':'localhost',
-        'PORT':'3306',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
 
